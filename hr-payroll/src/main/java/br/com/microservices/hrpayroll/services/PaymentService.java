e09ff234c3b7d54492974ce3d4c8578b0eb20d18
Guutooh <gustavo.oliveiradossantos@gmail.com>
2023-08-07 10:50:23 -0300

    Mudando de REstTemplate e Implementacao FeignCloud

--- hr-payroll/src/main/java/br/com/microservices/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/microservices/hrpayroll/services/PaymentService.java
@@ -2,30 +2,21 @@ package br.com.microservices.hrpayroll.services;
 
 import br.com.microservices.hrpayroll.entities.Payment;
 import br.com.microservices.hrpayroll.entities.Worker;
+import br.com.microservices.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment (long workerId, int days){
 
-        Map<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+ workerId);
 
-        Worker worker = restTemplate.getForObject(workerHost+"/worker/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
 
         return new Payment(worker.getNome(), worker.getDailyIncome(), days);
     }
--- hr-worker/src/main/java/br/com/microservices/hrworker/Controller/WorkerController.java
+++ hr-worker/src/main/java/br/com/microservices/hrworker/Controller/WorkerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/worker")
+@RequestMapping(value = "/workers")
 public class WorkerController {
 
     @Autowired
